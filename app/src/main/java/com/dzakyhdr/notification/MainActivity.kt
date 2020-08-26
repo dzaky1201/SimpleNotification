5b1e555cdf65cc1342d62698752d3fd7fcf7f1ab
Dzaky Haidar <dzakyhdr@gmail.com>
2020-08-26 15:15:56 +0700

    Notification

--- app/src/main/java/com/dzakyhdr/notification/MainActivity.kt
+++ app/src/main/java/com/dzakyhdr/notification/MainActivity.kt
@@ -2,13 +2,16 @@ package com.dzakyhdr.notification
 
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
 import android.graphics.Color
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
+import androidx.core.app.TaskStackBuilder
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -21,12 +24,21 @@ class MainActivity : AppCompatActivity() {
 
         createNotificatioChannel()
 
+        //intent notifikasi
+        val intent = Intent(this, MainActivity::class.java)
+        val pendingIntent = TaskStackBuilder.create(this).run {
+            addNextIntentWithParentStack(intent)
+                .getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT)
+        }
+
+        //notifikasi
         val notification = NotificationCompat.Builder(this, CHANNEL_ID)
-                .setContentTitle("Notifikasi ini berhasil")
-                .setContentText("Ini adalah konten dari notifikasi")
-                .setSmallIcon(R.drawable.ic_star)
-                .setPriority(NotificationCompat.PRIORITY_HIGH)
-                .build()
+            .setContentTitle("Notifikasi ini berhasil")
+            .setContentText("Ini adalah konten dari notifikasi")
+            .setSmallIcon(R.drawable.ic_star)
+            .setPriority(NotificationCompat.PRIORITY_HIGH)
+            .setContentIntent(pendingIntent)
+            .build()
 
         val notificationManager = NotificationManagerCompat.from(this)
         button.setOnClickListener {
@@ -35,13 +47,16 @@ class MainActivity : AppCompatActivity() {
 
     }
 
+    //cek versi untuk notifikasi
     fun createNotificatioChannel() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            val channel = NotificationChannel(CHANNEL_ID, CHANNEL_NAME,
-                    NotificationManager.IMPORTANCE_DEFAULT).apply {
-                        lightColor = Color.GREEN
-                        enableLights(true)
-                    }
+            val channel = NotificationChannel(
+                CHANNEL_ID, CHANNEL_NAME,
+                NotificationManager.IMPORTANCE_DEFAULT
+            ).apply {
+                lightColor = Color.GREEN
+                enableLights(true)
+            }
             val manager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
             manager.createNotificationChannel(channel)
         }
